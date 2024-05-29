a44c1bb9c9a3cd406d2eeef962a6e4d5caf1a8eb
Neil Munro <neil@elixirsoftware.co.uk>
2024-05-29 17:21:44 +0100

    Updated ex3.c code

--- ex3.c
+++ ex3.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 
+void crash() {
+  char *test = NULL;
+
+  for (int i = 0; i < 1000; i++) {
+    printf("%c", test[i]);
+  }
+}
+
 int main() {
   int age = 10;
   int height = 72;
 
   printf("I am %d years old.\n", age);
   printf("I am %d inches tall.\n", height);
+  crash();
 
   return 0;
 }
